2cd312e94e617f1c754456de03f7b097fb6cce34
Matheus <matheus.justino@compasso.com.br>
2021-07-06 19:51:15 -0300

    - criando proxy rbmq module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 // MODULES
 import { AppConfigModule } from './app-config/app-config.module';
 import { AuthModule } from './auth/auth.module';
+import { ProxyRbmqModule } from './proxy-rbmq/proxy-rbmq.module';
 
 @Module({
-	imports: [AppConfigModule, AuthModule],
+	imports: [AppConfigModule, AuthModule, ProxyRbmqModule],
 })
 export class AppModule {}
