9056aee5a17edb1396a5d0f71003a0e4b7570afa
Matheus <matheus.justino@compasso.com.br>
2021-07-06 19:38:53 -0300

    - iniciando auth module

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+
+// CONTROLLERS
 import { AuthController } from './auth.controller';
+
+// SERVICES
 import { AuthService } from './auth.service';
+
+// INTERFACES
+import { AUTH_SERVICE } from '@src/shared/interfaces/auth/auth.service';
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [
+		{
+			useClass: AuthService,
+			provide: AUTH_SERVICE,
+		},
+	],
+	exports: [
+		{
+			useClass: AuthService,
+			provide: AUTH_SERVICE,
+		},
+	],
 })
 export class AuthModule {}
